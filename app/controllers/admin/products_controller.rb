ab825ba967ebaf9a473a9d3aa1586811407861dd
kamitsuTW <kamitsu9103@gmail.com>
2020-11-19 15:31:13 +0800

    18 - add form for Product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,20 @@
 class Admin::ProductsController < Admin::BaseController
   def index
   end
+  
+  def new
+    @product = Product.new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
 end
