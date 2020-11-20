583b28fac191bc33f7638fa74dcdd93fae1cae13
kamitsuTW <kamitsu9103@gmail.com>
2020-11-20 11:55:50 +0800

    21 - Production listing, edit, and destroy

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < Admin::BaseController
+  before_action :find_product, only:[:edit, :update, :destroy]
   def index
+    @products = Product.all.includes(:vendor)
   end
   
   def new
@@ -20,17 +22,29 @@ class Admin::ProductsController < Admin::BaseController
   end
   
   def update
+    if @product.update(product_params)
+      redirect_to edit_admin_product_path(@product), notice: '商品已更新'
+    else
+      reder :edit
+    end
   end
   
   def destroy
+    @product.destroy
+    redirect_to admin_products_path, notice: "商品已刪除"
   end
 
   private
+
+  def find_product
+    @product = Product.friendly.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:name,
                                     :vendor_id,
                                     :list_price,
-                                    :sell_price)
+                                    :sell_price,
+                                    :on_sell)
   end
 end
