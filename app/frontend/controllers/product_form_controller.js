50973f3fbd58f9329a2540cb24633b7c70cc76db
kamitsuTW <kamitsu9103@gmail.com>
2020-11-23 16:49:32 +0800

    27 - add SKU part 4

--- app/frontend/controllers/product_form_controller.js
+++ app/frontend/controllers/product_form_controller.js
@@ -10,6 +10,15 @@ export default class extends Controller {
     this.linkTarget.insertAdjacentHTML('beforebegin', content);
   }
   
-  connect() {
+  remove_sku(event) {
+    event.preventDefault();
+
+    let wrapper = event.target.closest('.nested-fields');
+    if (wrapper.dataset.newRecord == 'true') {
+      wrapper.remove();
+    } else {
+      wrapper.querySelector("input[name*='_destroy']").value = 1;
+      wrapper.style.display = 'none';
+    }
   }
 }
