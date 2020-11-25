186eb0c66497efb506494a715e5397a8034fdb8c
kamitsuTW <kamitsu9103@gmail.com>
2020-11-25 17:29:08 +0800

    41 - shopping cart with TDD part 2

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,16 @@
 # PORO = Plain Old Ruby Object
 class Cart
+  attr_reader :items
+
+  def initialize
+    @item = []
+  end
+
   def add_item(product_id)
-    
+    @item << product_id
   end
 
   def empty?
-    false
+    @item.empty?
   end
 end
