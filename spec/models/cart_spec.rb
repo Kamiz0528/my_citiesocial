139200152098ab835a896659d9d7bb8f6b0b82bd
kamitsuTW <kamitsu9103@gmail.com>
2020-11-26 16:36:02 +0800

    49 - shopping cart with TDD part 10

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
+    let(:cart) { Cart.new }
+
   context "基本功能" do
     it "可以把商品丟到到購物車裡，然後購物車裡就有東西了" do
       # AAA = Arrange, Act, Assert
-      cart = Cart.new
       cart.add_item(2)
+
       expect(cart.empty?).to be false
+      
+      expect(cart).not_to be_empty
     end
 
     it "如果加了相同種類的商品到購物車裡，購買項目（CartItem）並不會增加，但商品的數量會改變" do
-      cart = Cart.new
       3.times { cart.add_item(1) }
       2.times { cart.add_item(2) }
 
@@ -19,7 +22,6 @@ RSpec.describe Cart, type: :model do
     end
 
     it "商品可以放到購物車裡，也可以再拿出來" do
-      cart = Cart.new
       # v1 = Vendor.create(title: 'v1')
       # p1 = Product.create(name: 'kk', list_price:10, sell_price: 5, vendor: v1)
       p1 = create(:product)
@@ -30,7 +32,6 @@ RSpec.describe Cart, type: :model do
     end
 
     it "可以計算整台購物車的總消費金額" do
-      cart = Cart.new
       p1 = create(:product, sell_price: 5)
       p2 = create(:product, sell_price: 10)
 
@@ -43,7 +44,6 @@ RSpec.describe Cart, type: :model do
 
   context "進階功能" do
     it "可以將購物車內容轉換成 Hash 並存到 Session 裡" do
-      cart = Cart.new
       p1 = create(:product)
       p2 = create(:product)
 
