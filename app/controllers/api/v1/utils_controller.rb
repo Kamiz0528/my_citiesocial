335ec49cba94eab54b495f0f00d3442992c4f150
kamitsuTW <kamitsu9103@gmail.com>
2020-11-27 16:40:49 +0800

    53 - Using shopping cart part 4

--- app/controllers/api/v1/utils_controller.rb
+++ app/controllers/api/v1/utils_controller.rb
@@ -15,12 +15,10 @@ class Api::V1::UtilsController < ApplicationController
     product = Product.friendly.find(params[:id])
 
     if product
-      cart = Cart.from_hash(session[:cart_123])
-      cart.add_item(product.code)
+      current_cart.add_item(product.code)
+      session[:cart_123] = current_cart.serialize
 
-      session[:cart_123] = cart.serialize
-
-      render json: {status: 'ok', items: cart.items.count}
+      render json: {status: 'ok', items: current_cart.items.count}
     end
   end
 end
